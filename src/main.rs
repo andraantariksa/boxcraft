7cde3d50f658ce41101023320258af682013e4e2
Andra Antariksa <andra.antariksa@gmail.com>
2022-06-09 16:05:36 +0700

    Add forgotten files to commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+pub mod game;
+pub mod input;
+pub mod physics;
+pub mod renderer;
+
+use crate::game::Game;
+use crate::input::InputManager;
+use renderer::renderer::Renderer;
+use std::rc::Rc;
+use std::time::Instant;
+
+fn main() {
+    env_logger::init();
+
+    let game = Game::new();
+    game.run_loop();
+}
